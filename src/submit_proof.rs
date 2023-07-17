67966affd67e7a5b07e8d2826b216ec641b6f29f
sifnoc <sifnoc09@gmail.com>
2023-07-17 07:08:15 +0000

    feat: print log if the proof is valid

--- src/submit_proof.rs
+++ src/submit_proof.rs
@@ -3,23 +3,34 @@ use std::{str::FromStr, sync::Arc};
 use ethers::{
     abi::Address,
     prelude::SignerMiddleware,
-    providers::{Http, Middleware, Provider},
-    signers::LocalWallet,
+    providers::{Http, Middleware, Provider, StreamExt},
+    signers::{LocalWallet, Signer},
     types::Bytes,
 };
 use halo2_proofs::halo2curves::bn256::Fr as Fp;
 
 use summa_backend::apis::snapshot::Snapshot;
 
-use super::summa_contract::summa::Summa;
+use super::summa_contract::summa::{
+    ExchangeAddressesSubmittedFilter, ProofOfSolvencySubmittedFilter, Summa,
+};
+
+fn get_contract_instance(
+    client: &SignerMiddleware<Provider<Http>, LocalWallet>,
+    contract_address: &str,
+) -> Summa<SignerMiddleware<Provider<Http>, LocalWallet>> {
+    let arc_client = Arc::new(client.clone());
+    let contract_address = Address::from_str(contract_address).unwrap();
+    Summa::new(contract_address, arc_client)
+}
 
 pub async fn generate_proof_of_solvency(
     snapshot: &Snapshot<4, 6, 2, 8>,
     client: &SignerMiddleware<Provider<Http>, LocalWallet>,
-) {
-    let client2 = Arc::new(client.clone());
-    let contract_address = Address::from_str("0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512").unwrap();
-    let summa_contract = Summa::new(contract_address, client2);
+) -> Result<(), Box<dyn std::error::Error>> {
+    // TODO: get contract address from config
+    let summa_contract =
+        get_contract_instance(client, "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512");
 
     let ownership_data = snapshot.get_proof_of_account_ownership();
     let asset_addresses = ownership_data.get_addresses(); // No needed actually
@@ -37,6 +48,11 @@ pub async fn generate_proof_of_solvency(
     let public_inputs = solvency_data.get_public_inputs();
     let proof: &Bytes = solvency_data.get_proof_calldata();
 
+    let block_number = client.get_block_number().await.unwrap();
+    let event = summa_contract
+        .event::<ProofOfSolvencySubmittedFilter>()
+        .from_block(block_number);
+
     summa_contract
         .submit_proof_of_solvency(
             vec![mock_erc_20_address],
@@ -48,19 +64,25 @@ pub async fn generate_proof_of_solvency(
         .await
         .unwrap();
 
-    println!("the proof has been validated!");
+    let mut stream = event.stream().await?.with_meta().take(1);
+    while let Some(Ok((event, meta))) = stream.next().await {
+        println!("The proof has been validated ✅");
+        println!("        mst_root: 0x{:02x}", event.mst_root);
+        println!("transaction hash: {:?}", meta.transaction_hash);
+    }
+
+    Ok(())
 }
 
 pub async fn generate_proof_of_ownership(
     snapshot: &Snapshot<4, 6, 2, 8>,
     client: &SignerMiddleware<Provider<Http>, LocalWallet>,
-) {
-    let ownership_data = snapshot.get_proof_of_account_ownership();
-    let client2 = Arc::new(client.clone());
-    let contract_address = Address::from_str("0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512").unwrap();
-    let summa_contract = Summa::new(contract_address, client2);
+) -> Result<(), Box<dyn std::error::Error>> {
+    let summa_contract =
+        get_contract_instance(client, "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512");
 
     // Converting types to be compatible with the contract
+    let ownership_data = snapshot.get_proof_of_account_ownership();
     let addresses = ownership_data.get_addresses();
 
     let mut cex_addresses = Vec::<Address>::new();
@@ -69,7 +91,6 @@ pub async fn generate_proof_of_ownership(
     }
 
     let signatures = ownership_data.get_signatures();
-
     let mut cex_signatures = Vec::<Bytes>::new();
     for sig in signatures {
         cex_signatures.push(Bytes::from_str(sig).unwrap())
@@ -77,11 +98,26 @@ pub async fn generate_proof_of_ownership(
 
     let message = ownership_data.get_message();
 
+    let block_number = client.get_block_number().await.unwrap();
+    let event = summa_contract
+        .event::<ExchangeAddressesSubmittedFilter>()
+        .from_block(block_number);
+
     summa_contract
         .submit_proof_of_account_ownership(cex_addresses, cex_signatures, message.to_string())
         .send()
         .await
         .unwrap();
 
-    println!("the proof has been validated!");
+    let mut stream = event.stream().await?.with_meta().take(1);
+    while let Some(Ok((event, meta))) = stream.next().await {
+        println!("The proof has been validated ✅");
+        println!(" CEX addresses:");
+        for (i, addr) in event.cex_addresses.iter().enumerate() {
+            println!("  {}: {:?}", i, addr);
+        }
+        println!("transaction hash: {:?}", meta.transaction_hash);
+    }
+
+    Ok(())
 }
